2e9360af0b1e49ccacb8c20e3d4b811c43da0699
luchivanich <luchivanich@gmail.com>
2020-03-20 23:53:36 +0300

    added couple of tests

--- AimTimersTests/AimTimerTests.cs
+++ AimTimersTests/AimTimerTests.cs
@@ -12,7 +12,7 @@ namespace AimTimersTests
     public class AimTimerTests
     {
         [Fact]
-        public void StartMethod_Creates_AimTimerItem_With_Correct_AimTimerInterval()
+        public void Start_Method_Creates_AimTimerItem_With_Correct_AimTimerInterval()
         {
             var now = DateTime.Now;
             var dateTimeProvider = new Mock<IDateTimeProvider>();
@@ -30,7 +30,7 @@ namespace AimTimersTests
         }
 
         [Fact]
-        public void StartMethod_Continues_Existing_AimTimerItem()
+        public void Start_Method_Continues_Existing_AimTimerItem()
         {
             var now = DateTime.Now;
             var dateTimeProvider = new Mock<IDateTimeProvider>();
@@ -52,6 +52,62 @@ namespace AimTimersTests
             Assert.Single(aimTimerItemModel.AimTimerIntervals);
             Assert.Equal(now, aimTimerItemModel.AimTimerIntervals.Single().StartDate);
             Assert.Null(aimTimerItemModel.AimTimerIntervals.Single().EndDate);
+        }
+
+        [Fact]
+        public void Start_Method_Does_Nothing_When_Timer_Runs()
+        {
+            var now = DateTime.Now;
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            dateTimeProvider.Setup(dtp => dtp.GetNow()).Returns(now);
+
+            var aimTimerIntervalModel = new AimTimerIntervalModel { StartDate = now };
+
+            var aimTimerItemModel = new AimTimerItemModel(now.AddHours(-2), now.AddHours(2))
+            {
+                AimTimerIntervals = new List<AimTimerIntervalModel> { aimTimerIntervalModel }
+            };
+
+            var aimTimerModel = new AimTimerModel
+            {
+                AimTimerItemModels = new List<AimTimerItemModel> { aimTimerItemModel }
+            };
+
+            var aimTimer = new AimTimer(aimTimerModel, dateTimeProvider.Object);
+
+            aimTimer.Start();
+
+            Assert.Single(aimTimerModel.AimTimerItemModels);
+            Assert.Equal(aimTimerItemModel, aimTimerModel.AimTimerItemModels.Single());
+            Assert.Single(aimTimerItemModel.AimTimerIntervals);
+            Assert.Equal(now, aimTimerItemModel.AimTimerIntervals.Single().StartDate);
+            Assert.Single(aimTimerItemModel.AimTimerIntervals);
+            Assert.Equal(aimTimerIntervalModel, aimTimerItemModel.AimTimerIntervals.Single());
+            Assert.Null(aimTimerItemModel.AimTimerIntervals.Single().EndDate);
+        }
+
+        [Fact]
+        public void Stop_Method_Set_EndTime_For_Current_Interval()
+        {
+            var now = DateTime.Now;
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            dateTimeProvider.Setup(dtp => dtp.GetNow()).Returns(now);
+
+            var aimTimerIntervalModel = new AimTimerIntervalModel { StartDate = now.AddHours(-1), EndDate = null };
+            var aimTimerItemModel = new AimTimerItemModel(now.AddHours(-2), now.AddHours(1))
+            {
+                AimTimerIntervals = new List<AimTimerIntervalModel> { aimTimerIntervalModel }
+            };
+            var aimTimerModel = new AimTimerModel
+            {
+                AimTimerItemModels = new List<AimTimerItemModel> { aimTimerItemModel }
+            };
+
+            var aimTimer = new AimTimer(aimTimerModel, dateTimeProvider.Object);
+
+            aimTimer.Stop();
+
+            Assert.Equal(now, aimTimerIntervalModel.EndDate);
         }
     }
 }
